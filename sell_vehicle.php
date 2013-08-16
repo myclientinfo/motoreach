94e45749024df6a5c110ebd2e0959538f832f7c3
Matt Burgess <matt@myclient.info>
2013-08-16 23:52:55 +1000

    Correct location variable

--- sell_vehicle.php
+++ sell_vehicle.php
@@ -12,7 +12,8 @@ if(isset($_POST['formdata']) && $_POST['formdata'] != 'miniform'){
 	$user = new Extend_User(false, false, true, 'auction_users');
 	$_POST['signup_time'] = date('Y-m-d H:i:s');
 	$_POST['statusID'] = 6;
-	$_POST['location_id'] = User::getRegion($_POST['zip']);
+	
+	$_POST['location_id'] = $_POST['city'];
 	
 	if($_POST['description'] == 'optional') $_POST['description'] = '';
 	
@@ -22,6 +23,8 @@ if(isset($_POST['formdata']) && $_POST['formdata'] != 'miniform'){
 	
 	$_POST['userID'] = $user_id;
 	$_POST['id'] = $user_id;
+	
+	//print_r($user_id);
 	
 	$content = new VehicleDetails(false, false, true, 'vehicle_details');
 	
@@ -33,33 +36,6 @@ if(isset($_POST['formdata']) && $_POST['formdata'] != 'miniform'){
 	$return_page = 'http://'.$_SERVER['HTTP_HOST'].'/confirm_sale.php';
 	
 	header('location: confirm_sale.php?auction_id='.$id);
-	die();
-	//$processor = $_SESSION['l10n']['processor'];
-	/*
-	if ($_SERVER['HTTP_HOST']=='motopublic'){
-		$processor = 'paypal';
-	} else {
-		$processor = 'anz';
-	}
-	if($processor == 'anz')	{
-		$credit_string = Credit::createCCString($_SESSION['l10n']['rate_public_list'], $id, $return_page, $desc);
-		header('location:'.$credit_string);
-	} elseif($processor == 'paypal') {
-		 //CallShortcutExpressCheckout( $paymentAmount, $currencyCodeType, $paymentType, $returnURL, $cancelURL, $payerID) {
-		$resArray = Credit::CallShortcutExpressCheckout($_SESSION['l10n']['rate_public_list'], $_SESSION['l10n']['currency'], 'sale', $return_page, $return_page.'&failure', $id.'-'.$user_id); 
-		$ack = strtoupper($resArray["ACK"]);
-		
-		if($ack=="SUCCESS" || $ack=="SUCCESSWITHWARNING"){
-			Credit::RedirectToPayPal ( $resArray["TOKEN"], $id );
-		} else {
-			header('location:/confirm_sale.php?auction_id='.$id.'&failure');
-		}
-
-	}
-	
-	*/
-	
-	
 	die();
 }
 
